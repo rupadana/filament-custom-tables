b4867ba35b268e912abe6644e89ffaa106a79bcd
I Wayan Rupadana <rupadanawayan@gmail.com>
2023-10-09 17:43:44 +0800

    change: Using countdown.js to handle timeago

--- resources/js/components/timeago.js
+++ resources/js/components/timeago.js
@@ -1,3 +1,5 @@
+
+import countdown from "./countdown.min.cjs";
 export default function timeago({
     strdate,
     id,
@@ -14,49 +16,13 @@ export default function timeago({
         prefix,
         suffix,
         getTimeAgo() {
-            var date1 = new Date();
-            var date2 = new Date(this.strdate * 1000)
-
-            let time = (date2.getTime() - date1.getTime()) < 0 ? false : true, // work out which is the later date
-                laterDate = time ? date2 : date1,
-                earlierDate = time ? date1 : date2,
-                secondDiff = laterDate.getSeconds() - earlierDate.getSeconds(),
-                minuteDiff = laterDate.getMinutes() - earlierDate.getMinutes(),
-                hourDiff = laterDate.getHours() - earlierDate.getHours(),
-                dayDiff = laterDate.getDate() - earlierDate.getDate(),
-                monthDiff = laterDate.getMonth() - earlierDate.getMonth(), // javascript uses zero-indexed months (0-11 rather than the more conventional 1-12)
-                yearDiff = laterDate.getFullYear() - earlierDate.getFullYear(),
-                months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-
-            if (secondDiff < 0) {
-                minuteDiff--;
-                secondDiff = 60 + secondDiff
-            }
-
-            if (minuteDiff < 0) {
-                hourDiff--;
-                minuteDiff = 60 + minuteDiff
-            }
-
-            if (hourDiff < 0) {
-                dayDiff--;
-                hourDiff = 24 + hourDiff
-            }
-
-            if (dayDiff < 0) {
-                monthDiff--;
-                dayDiff += months[laterDate.getMonth() - 1]; // -1 because we want the previous month
-            }
-
-            if (monthDiff < 0) {
-                yearDiff--;
-                monthDiff += 12;
-            }
+            const date1 = new Date();
+            const date2 = new Date(this.strdate * 1000)
+            let diff = countdown(date2, date1)
+            const difference = [diff.years, diff.months, diff.days, diff.hours, diff.minutes, diff.seconds];
 
             let datestring = '';
-
-            [yearDiff, monthDiff, dayDiff, hourDiff, minuteDiff, secondDiff].forEach((val, i) => {
+            difference.forEach((val, i) => {
                 if (val > 0) {
                     if (datestring != '') {
                         datestring += ' ';
@@ -65,14 +31,15 @@ export default function timeago({
                 }
             })
 
-            if(this.prefix) datestring = this.prefix + ' ' + datestring;
+            if (this.prefix) datestring = this.prefix + ' ' + datestring;
 
             if (this.suffix) datestring = datestring + ' ' + this.suffix;
-            
-            return datestring;
+            return datestring
+
         },
         init() {
             document.getElementById(this.id).innerHTML = this.getTimeAgo()
+            this.getTimeAgo();
             setInterval(() => {
                 document.getElementById(this.id).innerHTML = this.getTimeAgo()
             }, this.interval);
